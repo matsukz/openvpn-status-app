759ff28dcb0b5dd4fb7a0d2e1d398adb5b0b94ae
まつ <92625267+matsukz@users.noreply.github.com>
2024-11-11 11:33:44 +0900

    再起動関数を追加しました

---
+++ fastapi/extend/system_operation.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def openvpn_restart(service_name) -> bool:
+    result = subprocess.run(
+        ["systemctl", "restart", service_name],
+        capture_output=True,
+        text=True
+    )
+    if result.returncode == 0:
+        return True
+    else:
+        print(f"{service_name} Restart Fail. {result.stderr}")
+        return False
