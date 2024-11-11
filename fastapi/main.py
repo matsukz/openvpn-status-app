563331110bf4c6c37629e0bbf77949d453938263
まつ <92625267+matsukz@users.noreply.github.com>
2024-11-11 22:57:12 +0900

    空白削除

--- fastapi/main.py
+++ fastapi/main.py
@@ -23,8 +23,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],   
-    allow_headers=["*"]    
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 @app.on_event("startup")
