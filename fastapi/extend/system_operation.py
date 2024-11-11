9b9b60797b597df16b8ecac3a6d7d7de5942671e
まつ <92625267+matsukz@users.noreply.github.com>
2024-11-11 13:05:05 +0900

    起動と停止用の関数を追加しました

--- fastapi/extend/system_operation.py
+++ fastapi/extend/system_operation.py
@@ -1,8 +1,32 @@
 import subprocess
 
+def start(service_name) -> bool:
+    result = subprocess.run(
+        ["systemctl", "start", service_name],
+        capture_output=True,
+        text=True
+    )
+    if result.returncode == 0:
+        return True
+    else:
+        print(f"{service_name} Restart Fail. {result.stderr}")
+        return False
+
 def restart(service_name) -> bool:
     result = subprocess.run(
         ["systemctl", "restart", service_name],
+        capture_output=True,
+        text=True
+    )
+    if result.returncode == 0:
+        return True
+    else:
+        print(f"{service_name} Restart Fail. {result.stderr}")
+        return False
+    
+def stop(service_name) -> bool:
+    result = subprocess.run(
+        ["systemctl", "stop", service_name],
         capture_output=True,
         text=True
     )
