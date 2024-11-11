f5de44f25634327cd77f0527d5185d61f2e08463
まつ <92625267+matsukz@users.noreply.github.com>
2024-11-11 23:09:55 +0900

    開始と停止するAPIをコールする

--- html/operation_button.js
+++ html/operation_button.js
@@ -27,5 +27,32 @@ function put_reboot(){
 
 function btn_start(){
 
+    const server_status = document.getElementById("btn_start").textContent;
+    let action_query = "";
+    let confirm = false;
+    switch(server_status){
+        case "開始":
+            action_query = "start";
+            confirm = window.confirm("OpenVPNを起動しますか？");
+            break;
+        default:
+            action_query = "stop";
+            confirm = window.confirm("OpenVPNを停止しますか？\n【警告】全てのセッションが切断されます");
+            break
+    }
 
+    var url = new URL(window.location.href);
+    var host = url.hostname;
+
+    $.ajax({
+        type: "PUT",
+        url: "http://" + host + ":9004/ovpn/api/status/?action=" + action_query,
+        cache: false 
+    }).done(function(response){
+       alert("操作が完了しました。\n※反映には時間を要する場合があります");
+       location.reload()
+    }).fail(function(response){
+        alert("リクエストに失敗しました");
+        console.log(response)
+    })
 }
